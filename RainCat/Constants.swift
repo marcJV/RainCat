25ba498b9f7d515370978f427dc2fbbf7875b67c
MarcJV <marc.vandehey@gmail.com>
2016-08-30 15:26:54 -0500

    Day Two Completed

---
+++ RainCat/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RainCat
+//
+//  Created by Marc Vandehey on 8/30/16.
+//  Copyright © 2016 Thirteen23. All rights reserved.
+//
+
+let RainDropCategory   : UInt32 = 0x1 << 1
+let FloorCategory      : UInt32 = 0x1 << 2
+let UmbrellaCategory   : UInt32 = 0x1 << 3
+let WorldFrameCategory : UInt32 = 0x1 << 4
--- RainCat/Scenes/GameScene.swift
+++ RainCat/Scenes/GameScene.swift
@@ -9,30 +9,57 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
   private var lastUpdateTime : TimeInterval = 0
   private var currentRainDropSpawnTime : TimeInterval = 0
   private var rainDropSpawnRate : TimeInterval = 0.5
   private let random = GKARC4RandomSource()
 
+  private let umbrella = UmbrellaSprite.newInstance()
+
   override func sceneDidLoad() {
     self.lastUpdateTime = 0
+
+    var worldFrame = frame
+    worldFrame.origin.x -= 100
+    worldFrame.origin.y -= 100
+    worldFrame.size.height += 200
+    worldFrame.size.width += 200
+
+    self.physicsBody = SKPhysicsBody(edgeLoopFrom: worldFrame)
+    self.physicsWorld.contactDelegate = self
+    self.physicsBody?.categoryBitMask = WorldFrameCategory
 
     let floorNode = SKShapeNode(rectOf: CGSize(width: size.width, height: 5))
     floorNode.position = CGPoint(x: size.width / 2, y: 50)
     floorNode.fillColor = SKColor.red
     floorNode.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: -size.width / 2, y: 0), to: CGPoint(x: size.width, y: 0))
+    floorNode.physicsBody?.categoryBitMask = FloorCategory
+    floorNode.physicsBody?.contactTestBitMask = RainDropCategory
+    floorNode.physicsBody?.restitution = 0.3
+
     addChild(floorNode)
+
+    umbrella.updatePosition(point: CGPoint(x: frame.midX, y: frame.midY))
+    addChild(umbrella)
   }
 
 
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    let touchPoint = touches.first?.location(in: self)
 
+    if let point = touchPoint {
+      umbrella.setDestination(destination: point)
+    }
   }
 
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    let touchPoint = touches.first?.location(in: self)
 
+    if let point = touchPoint {
+      umbrella.setDestination(destination: point)
+    }
   }
 
   override func update(_ currentTime: TimeInterval) {
@@ -55,6 +82,8 @@ class GameScene: SKScene {
       spawnRaindrop()
     }
 
+    umbrella.update(deltaTime: dt)
+
     self.lastUpdateTime = currentTime
   }
 
@@ -63,12 +92,32 @@ class GameScene: SKScene {
     rainDrop.position = CGPoint(x: size.width / 2, y:  size.height / 2)
     rainDrop.fillColor = SKColor.blue
     rainDrop.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 20, height: 20))
+    rainDrop.physicsBody?.categoryBitMask = RainDropCategory
+    rainDrop.physicsBody?.contactTestBitMask = WorldFrameCategory
 
     let randomPosition = abs(CGFloat(random.nextInt()).truncatingRemainder(dividingBy: size.width))
     rainDrop.position = CGPoint(x: randomPosition, y: size.height)
 
-    print("position: \(randomPosition)")
-
     addChild(rainDrop)
+  }
+
+  func didBegin(_ contact: SKPhysicsContact) {
+    if (contact.bodyA.categoryBitMask == RainDropCategory) {
+      contact.bodyA.node?.physicsBody?.collisionBitMask = 0
+      contact.bodyA.node?.physicsBody?.categoryBitMask = 0
+    } else if (contact.bodyB.categoryBitMask == RainDropCategory) {
+      contact.bodyB.node?.physicsBody?.collisionBitMask = 0
+      contact.bodyB.node?.physicsBody?.categoryBitMask = 0
+    }
+
+    if contact.bodyA.categoryBitMask == WorldFrameCategory {
+      contact.bodyB.node?.removeFromParent()
+      contact.bodyB.node?.physicsBody = nil
+      contact.bodyB.node?.removeAllActions()
+    } else if contact.bodyB.categoryBitMask == WorldFrameCategory {
+      contact.bodyA.node?.removeFromParent()
+      contact.bodyA.node?.physicsBody = nil
+      contact.bodyA.node?.removeAllActions()
+    }
   }
 }
